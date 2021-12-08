4eb3eedc4726669b5df4f30206577279b1dff253
Rodrigo Romualdo <rodrigo.s.romualdo@gmail.com>
2021-12-08 00:38:23 -0300

    modulo mercado pago

--- src/routes/checkout.mercadopago.routes.js
+++ src/routes/checkout.mercadopago.routes.js
@@ -3,22 +3,45 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 //const temaNivel = require('../models/temaNivel');
-
 const db = mongoose.connection;
-//const session = await db.startSession();
+
+var mercadopago = require('mercadopago');
+mercadopago.configure({
+   access_token: process.env.ENV_ACCESS_TOKEN
+});
 
 router.post('/pagamento', async (req, res) => {
 
    try {
       console.log(req.body)
-      res.send({"error": false, "message": "RETORNO DO CHECKOUT"})
-      res.status(201).end()
+
+      let preference = {
+         items: [{
+            title: 'Blue shirt',
+            quantity: 10,
+            currency_id: 'BRL',
+            unit_price: 10
+            }],
+         payer: {
+            email: "demo@mail.com"
+         },
+         payment_methods: {
+            installments: 1
+         }
+      };
+
+      mercadopago.preferences.create(preference).then(function (data) {
+         console.log('data: ',data)
+         res.json(data.response.sandbox_init_point)
+         res.status(201).end()
+      }).catch(function (error) {
+         console.log('error: ', error)
+      });
+
+
+
+      
     
-
-    //res.json({"error": false, "message": "Chegou o MP"})
-    //res.status(201).end()
-
-  
      
    } catch (error) {
       console.log(error)
